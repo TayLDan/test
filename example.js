51e194f7c8fe867ea147f3227caf105509e96b6d
TayLDan <ilovedan@TayLDan.com>
2017-01-18 16:35:20 +0800

    something has changed

---
+++ example.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Example</title>
+    <script>
+        JavaScript goes here...
+    </script>
+</head>
+<body>
+    Mark-up goes here...
+</body>
+</html>
